ae8e26e1e75255b8ff34ed7b3c6e2beecb5e9a41
viki <viki@ROS.(none)>
2013-04-09 14:36:55 -0500

    modified face state and added new states and transitions

--- src/taggraph.py
+++ src/taggraph.py
@@ -55,7 +55,7 @@ class ydState(smach.State):
 class GetNode(ydState):
     def __init__(self):
         ydState.__init__(self)
-        smach.State.__init__(self, outcomes=['no_node','acquired_node'])
+        smach.State.__init__(self, outcomes=['no_node','acquired_node', 'land', 'take_off'])
         
 
     def execute(self, userdata):
@@ -66,14 +66,47 @@ class GetNode(ydState):
             
     def handleStatus(self, msg):
       return
+      
+''' define TakeOff state ******************************************************'''
+class TakeOffNode(ydState):
+    def __init__(self):
+        ydState.__init__(self)
+        smach.State.__init__(self, outcomes=['taking_off','in_flight'])
+        
+
+    def execute(self, userdata):
+        if True:
+            return 'in_flight'
+        else:
+            return 'taking_off'
+            
+    def handleStatus(self, msg):
+      return
+      
+''' define Landing state ******************************************************'''
+class LandingNode(ydState):
+    def __init__(self):
+        ydState.__init__(self)
+        smach.State.__init__(self, outcomes=['landing','landed'])
+        
+
+    def execute(self, userdata):
+        if False:
+            return 'landing'
+        else:
+            return 'landed'
+            
+    def handleStatus(self, msg):
+      return
 
 ''' define searchNode state ******************************************************'''
 class SearchNode(ydState):
     def __init__(self):
         ydState.__init__(self)
-        smach.State.__init__(self, outcomes=['still_lost','acquired_node'])
+        smach.State.__init__(self, outcomes=['still_lost','acquired_node', 'gave_up'])
         self.searchIsRunning = False
-        self.commandID = -1	
+        self.commandID = -1
+        self.giveUp = False
         
         m = commandsMsg()
         self.pubCommands.publish(m) # send empty message as initialization
@@ -82,7 +115,7 @@ class SearchNode(ydState):
         self.hasAcquiredNode = False
         
         # execute search command
-        if self.searchIsRunning == False and self.hasAcquiredNode == False:
+        if self.searchIsRunning == False and self.hasAcquiredNode == False and self.giveUp == False:
 	  m=commandsMsg()
 	  m.hasSearch=True
 	  m.tagNr=self.getTagNr()
@@ -93,66 +126,68 @@ class SearchNode(ydState):
 	if self.searchIsRunning == True and self.hasAcquiredNode == False:
 	  rospy.loginfo("still searching for tag nr %i"%self.getTagNr())
 	
-	# busy wait for result
-        while (True):
-	  rospy.sleep(0.5)
-	  if self.hasAcquiredNode == False:
-            return 'still_lost'
-          else:
-            return 'acquired_node'
+	rospy.sleep(0.5)
+	if self.giveUp == True:
+	  return 'gave_up'
+	elif self.hasAcquiredNode == False:
+          return 'still_lost'
+        else:
+          return 'acquired_node'
     
     def handleStatus(self, msg):
 	rospy.loginfo("Command status changed: (id=%i) %s"%(msg.id, msg.status))
 	if msg.id == -1 and msg.status == "available":
 	  # search was finished without success
 	  self.searchIsRunning = False
+	  self.giveUp = True
         if msg.id == self.commandID and msg.status == "done_successfully":
 	  # search was finished successfully
 	  self.hasAcquiredNode = True
-	  self.setToNextTag()
 
 
 ''' define faceNode state ******************************************************'''
 class FaceNode(ydState):
     def __init__(self):
         ydState.__init__(self)
-        smach.State.__init__(self, outcomes=['lost','done'])
-        self.searchIsRunning = False
-        self.commandID = -1	
+        smach.State.__init__(self, outcomes=['lost','done', 'control'])
+        self.faceIsRunning = False
+        self.commandID = -1
+        self.isLost = False
         
         m = commandsMsg()
         self.pubCommands.publish(m) # send empty message as initialization
 	
     def execute(self, userdata):
         # execute face command
-        
-	m=commandsMsg()
-	m.hasFace=True
-	m.tagNr=self.getTagNr()
-	self.pubCommands.publish(m)
-	self.commandID = m.header.seq
-	rospy.loginfo("search command submitted: id = %i"%self.commandID)
-	self.searchIsRunning = True
-	if self.searchIsRunning == True and self.hasAcquiredNode == False:
-	  rospy.loginfo("still searching for tag nr %i"%self.getTagNr())
+        if self.faceIsRunning == False and self.isFacing == False and self.isLost == False:
+	  m=commandsMsg()
+	  m.hasFace=True
+	  m.tagNr=self.getTagNr()
+	  self.pubCommands.publish(m)
+	  self.commandID = m.header.seq
+	  rospy.loginfo("face command submitted: id = %i"%self.commandID)
+	  self.faceIsRunning = True
+	if self.faceIsRunning == True and self.isFacing == False:
+	  rospy.loginfo("still attempting to face tag nr %i"%self.getTagNr())
 	
-	# busy wait for result
-        while (True):
-	  rospy.sleep(0.5)
-	  if self.hasAcquiredNode == False:
-            return 'still_lost'
-          else:
-            return 'acquired_node'
+	rospy.sleep(0.5)
+	if self.isLost == True:
+	  return 'lost'
+	elif self.isFacing == True:
+	  #send out reset message
+          return 'done'
+        else:
+          return 'control'
     
     def handleStatus(self, msg):
 	rospy.loginfo("Command status changed: (id=%i) %s"%(msg.id, msg.status))
 	if msg.id == -1 and msg.status == "available":
-	  # search was finished without success
-	  self.searchIsRunning = False
-        if msg.id == self.commandID and msg.status == "done_successfully":
+	  # face was finished without success
+	  self.faceIsRunning = False
+	  self.isLost = True
+        if msg.id == self.commandID and msg.status == "stable":
 	  # search was finished successfully
-	  self.hasAcquiredNode = True
-	  self.setToNextTag()
+	  self.isFacing = True
 	  
 	  
 ''' define TargetApproachNode state ******************************************************'''
@@ -171,7 +206,7 @@ class TargetApproachNode(ydState):
 class DockNode(ydState):
     def __init__(self):
         ydState.__init__(self)
-        smach.State.__init__(self, outcomes=['docked','lost'])
+        smach.State.__init__(self, outcomes=['docked','lost', 'docking'])
     
     def execute(self, userdata):
         if True:
@@ -186,7 +221,7 @@ class DockNode(ydState):
 class HoverNode(ydState):
     def __init__(self):
         ydState.__init__(self)
-        smach.State.__init__(self, outcomes=['finished'])
+        smach.State.__init__(self, outcomes=['finished', 'hovering'])
     
     def execute(self, userdata):
         return 'finished'
@@ -205,25 +240,36 @@ def main():
     # Open the container
     with sm:
         # Add states to the container
-
         smach.StateMachine.add('GET', GetNode(), 
-                               transitions={'no_node':'GET', 
+                               transitions={'no_node':'GET',
+                               'take_off':'TAKEOFF',
+                               'land':'LAND',
                                'acquired_node':'SEARCH'})
+        smach.StateMachine.add('TAKEOFF', TakeOffNode(), 
+                               transitions={'taking_off':'TAKEOFF', 
+                               'in_flight':'GET'})
+        smach.StateMachine.add('LAND', LandingNode(), 
+                               transitions={'landing':'LAND', 
+                               'landed':'GET'})
         smach.StateMachine.add('SEARCH', SearchNode(), 
                                transitions={'still_lost':'SEARCH',
-                               'acquired_node':'FACE'})
+                               'acquired_node':'FACE',
+                               'gave_up':'gave_up'})
         smach.StateMachine.add('FACE', FaceNode(), 
                                transitions={'lost':'SEARCH',
+                               'control':'FACE',
                                'done':'TARGET_APPROACH'})
         smach.StateMachine.add('TARGET_APPROACH', TargetApproachNode(), 
                                transitions={'still_tracking':'TARGET_APPROACH',
-                               'in_range':'DOCKING',
+                               'in_range':'DOCK',
                                'lost':'SEARCH'})        
-        smach.StateMachine.add('DOCKING', DockNode(), 
+        smach.StateMachine.add('DOCK', DockNode(), 
                                transitions={'docked':'HOVER',
+                               'docking':'DOCK',
                                'lost':'SEARCH'})
         smach.StateMachine.add('HOVER', HoverNode(), 
-                               transitions={'finished':'GET'})
+                               transitions={'finished':'GET',
+                               'hovering':'HOVER'})
     
     
     # Create and start the introspection server
